c87e4808375ed882c838c1d5669d918113c5c647
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-22 20:53:34 +0100

    Added Header Buttons

--- codebattles-web/src/components/Header.vue
+++ codebattles-web/src/components/Header.vue
@@ -2,7 +2,9 @@
 export default {}
 </script>
 <template>
-    <div className="navbar bg-base-300 flex justify-between">
+    <div className="navbar bg-base-300 flex items-baseline gap-3 p-0 px-2">
         <a className="normal-case text-xl font-inter font-bold px-2">>/&lt; CODE BATTLES</a>
+        <a className="h-full border-b-2 border-[#36D399]">Battles</a>
+        <a className="h-full border-b-2 border-[#A6ADBB]">Co-Op</a>
     </div>
 </template>
