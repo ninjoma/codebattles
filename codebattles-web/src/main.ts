dd1218a78083abe3251e4118d977cc058f7566bf
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-22 19:49:22 +0100

    Added Vue-CodeMirror

--- codebattles-web/src/main.ts
+++ codebattles-web/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Router from './router/index'
+import { basicSetup } from "vue-codemirror"
+
 
 createApp(App)
 
