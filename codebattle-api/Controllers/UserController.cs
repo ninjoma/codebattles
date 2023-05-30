7ae3a28b2383171e532b177540e6a7687fdfe580
David Torrubia <davidtorrubiasantos@gmail.com>
2023-05-30 23:18:45 +0200

    Cambios varios

--- codebattle-api/Controllers/UserController.cs
+++ codebattle-api/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using codebattle_api.DTO;
 using codebattle_api.Entities;
+using codebattle_api.Exceptions;
 using codebattle_api.Services.UserServices;
 using codebattle_api.utils;
 using Microsoft.AspNetCore.Mvc;
@@ -16,11 +17,15 @@ namespace codebattle_api.Controllers
 
         public override async Task<IActionResult> Create([FromBody] UserDTO postDTO)
         {
-            if (postDTO.Password != null){
-                postDTO.Password = PasswordHasher.HashPassword(postDTO.Password);
-                return Ok(await _service.Add(postDTO));
+            try {
+                if (postDTO.Password != null){
+                    postDTO.Password = PasswordHasher.HashPassword(postDTO.Password);
+                    return Ok(await _service.Add(postDTO));
+                }
+                return BadRequest();
+            } catch(CodeBattleException ex){
+                return ReturnError(ex);
             }
-            return BadRequest();
         }
 
     }
