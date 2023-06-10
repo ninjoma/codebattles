36705b420399108a8e3830b4202c9082a940b9f9
David Torrubia <davidtorrubiasantos@gmail.com>
2023-06-11 00:28:07 +0200

    Aun mas pruebas

--- codebattles-web/vite.config.ts
+++ codebattles-web/vite.config.ts
@@ -8,8 +8,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://codebattlesapi.azurewebsites.net/',
-        changeOrigin: false,
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+        ignorePath: true,
       }
     }
   }
