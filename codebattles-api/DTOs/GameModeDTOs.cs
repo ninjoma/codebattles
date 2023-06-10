c17c736d2f6adcb794ff789ceaef821118ce390b
Ninjam <juanjosegutierrez107@gmail.com>
2023-06-10 19:27:40 +0200

    Updated GameMode Players

--- codebattles-api/DTOs/GameModeDTOs.cs
+++ codebattles-api/DTOs/GameModeDTOs.cs
@@ -5,6 +5,7 @@ namespace codebattle_api.DTO
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int? Time { get; set; }
+        public int? MaxPlayers { get; set; }
     }
 
     public class GameModeDetailDTO : GameModeDTO
