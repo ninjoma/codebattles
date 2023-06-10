b575beae366476b60cd492c2a3b2119e41f31f21
David Torrubia <davidtorrubiasantos@gmail.com>
2023-06-11 01:41:13 +0200

    Descomentado proxyuy de juanjo

--- codebattles-web/vite.config.ts
+++ codebattles-web/vite.config.ts
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://codebattlesapi.azurewebsites.net/',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //       ignorePath: true,
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://codebattlesapi.azurewebsites.net/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        ignorePath: true,
+      }
+    }
+  }
 })
 
