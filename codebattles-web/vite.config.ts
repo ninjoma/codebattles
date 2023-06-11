25dfbe5cd17a1e09976b77fe1754eb784f33f7cf
David Torrubia <davidtorrubiasantos@gmail.com>
2023-06-11 17:01:19 +0200

    Comentado rewrite

--- codebattles-web/vite.config.ts
+++ codebattles-web/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://codebattlesapi.azurewebsites.net/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // rewrite: (path) => path.replace(/^\/api/, ''),
         headers: {
           "host": "codebattlesapi.azurewebsites.net",
         },
