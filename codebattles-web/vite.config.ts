53f0d7d43a3a1b7ce758459fe22f1fa79149988d
David Torrubia <davidtorrubiasantos@gmail.com>
2023-06-11 17:04:58 +0200

    Descomentado el Replace y comentado el changeOrigin

--- codebattles-web/vite.config.ts
+++ codebattles-web/vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://codebattlesapi.azurewebsites.net/',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+        // changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
         headers: {
           "host": "codebattlesapi.azurewebsites.net",
         },
